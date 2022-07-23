3572c2ab30b41978a85fe6a2836715cb76e8ec05
SugunaChandu <109806596+SugunaChandu@users.noreply.github.com>
2022-07-23 07:48:15 +0530

    Add files via upload

---
+++ program28.py
@@ -0,0 +1,10 @@
+#reverse elements of tuple
+a=list()
+n=int(input("enter the size of list:"))
+for i in range(n):
+    b=int(input("enter a element:"))
+    a.append(b)
+c=tuple(a)
+print("elements before reversing a tuple:",c)
+a.reverse()
+print("elements after reversing a tuple:",tuple(a))
